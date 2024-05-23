12d66e5c851b89723d0d3bcc4076a404343cd76a
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-05-23 10:33:13 +0700

    finish course

--- WebpackProject/src/index.js
+++ WebpackProject/src/index.js
@@ -10,7 +10,6 @@ import '../style/react-range.css';
 import '../style/style.css';
 const App = () => {
   const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
-
   return (
     <Provider store={store}>
       <Routes />
